ba38a9514c723c8ec7ee07cd1dfeaab477cee7e7
patrickcarmo <patrick.a.carmo@gmail.com>
2019-02-19 23:57:08 +0000

    Paragraph

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ const Header = props => {
 	return (
 		<div className="jumbotron">
 			<h1 className="display-4">Currency Converter</h1>
-			<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 		</div>
 	)
 }
